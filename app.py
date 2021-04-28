005d16e39c6a71244bdf7000cbfa2a2ce04f9c4d
ig_Dhanush <66821432+dhanush77777@users.noreply.github.com>
2021-04-28 14:52:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 
 amazon_scraper = AutoScraper()
-amazon_scraper.load('amazon-search1.json')
+amazon_scraper.load('amazon-search4.json')
 app = Flask(__name__)
 
 def get_amazon_result(search_query):
